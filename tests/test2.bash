6edcfd385f204d2fc6813887965f944e6844cff6
ChongChong He <che1234@umd.edu>
2023-06-13 12:03:56 -0700

    add test2.bash

--- tests/test2.bash
+++ tests/test2.bash
@@ -7,20 +7,18 @@ set -e
 # date and time format in EST: YYYY-MM-DD_HH:MM:SS_EST
 date_time=$(TZ=":US/Eastern" date +"%Y-%m-%d_%H:%M:%S_EST")
 folder_name="test_$date_time"
-mkdir -p "output/$folder_name"
-cd "output/$folder_name"
+mkdir -p "output/test2/$folder_name"
+cd "output/test2/$folder_name"
 
-# run the code
-# pwd 2>&1 >> log.txt
-
-for power in {3..11}
+for power in {1..4}
 do
-  N=$((2**$power))
+  N=$((2**($power+6)))
   echo "N = $N"
-  thedir="N$N"
+  input="0%power-nbody-p$N_m1.in"
+  thedir="0%power-nbody-p$N_m1.in"
   mkdir -p $thedir
   cd $thedir
-  ../../../src/gravidy-cpu -i ../../input/plummer/nbody_unit/plummer_N$N/gravidy.txt -o output.gravidy -t 50 2>&1 >> log.txt
+  ../../../../../src/gravidy-cpu -i ../../input/plummer/nbody_unit/plummer_N$N/gravidy.txt -o output.gravidy -t 50 2>&1 >> log.txt
   cd ..
 done
 
